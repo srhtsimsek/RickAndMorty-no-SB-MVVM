f3f03440378563f3245994bc33a7a4ee95600296
Serhat <srhtsimsekk@gmail.com>
2023-07-30 08:19:14 +0300

    Initial Commit

---
+++ RickAndMorty | no SB | MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty | no SB | MVVM
+//
+//  Created by Serhat  Şimşek  on 30.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
